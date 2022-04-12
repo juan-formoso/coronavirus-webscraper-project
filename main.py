1b36ef17efa5e512ee94f0eebc225d1196442678
Avery <trajano.juan@gmail.com>
2022-04-12 19:25:56 -0300

    Add class data

--- main.py
+++ main.py
@@ -5,6 +5,22 @@ API_KEY = "tqT3LV5LTVBd"
 PROJECT_TOKEN = "t4YV1yd2o04o"
 RUN_TOKEN = "tGKqm-Jp-rCm"
 
-response = requests.get(f'https://www.parsehub.com/api/v2/projects/{PROJECT_TOKEN}/last_ready_run/data', params={"api_key": API_KEY})
+class Data:
+  def __init__(self, api_key, project_token):
+    self.api_key = api_key
+    self.project_token = project_token
+    self.params = {'api_key': self.api_key}
+    self.get_data()
 
-data = json.loads(response.text)
+  def get_data(self):
+    response = requests.get(f'https://www.parsehub.com/api/v2/projects/{self.project_token}/last_ready_run/data', params=self.params)
+    self.data = json.loads(response.text)
+  
+  def get_total_cases(self):
+    data = self.data['total']
+    for content in data:
+      if content['name'] === 'Coronavirus Cases:':
+        return content['value']
+
+data = Data(API_KEY, PROJECT_TOKEN)
+print(data.data)
