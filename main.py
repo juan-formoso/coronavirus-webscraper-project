49a1b19db3d4e159f4c78f5422262e9164a99117
Avery <trajano.juan@gmail.com>
2022-04-12 19:19:46 -0300

    Add API URL

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import requests
+import json
 
 API_KEY = "tqT3LV5LTVBd"
 PROJECT_TOKEN = "t4YV1yd2o04o"
 RUN_TOKEN = "tGKqm-Jp-rCm"
+
+response = requests.get(f'https://www.parsehub.com/api/v2/projects/{PROJECT_TOKEN}/last_ready_run/data', params={"api_key": API_KEY})
+
+data = json.loads(response.text)
